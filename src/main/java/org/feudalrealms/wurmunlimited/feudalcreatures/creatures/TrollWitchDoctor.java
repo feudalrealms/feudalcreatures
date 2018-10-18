36d799ce6ec80d97df0a10caeb979ee8f03b8889
kalxen <41556473+kalxen@users.noreply.github.com>
2018-10-17 22:18:22 -0500

    normalized troll and textures

--- src/main/java/org/feudalrealms/wurmunlimited/feudalcreatures/creatures/TrollWitchDoctor.java
+++ src/main/java/org/feudalrealms/wurmunlimited/feudalcreatures/creatures/TrollWitchDoctor.java
@@ -1,6 +1,10 @@
 package org.feudalrealms.wurmunlimited.feudalcreatures.creatures;
 
+import java.util.Map;
+import java.util.logging.Logger;
+
 import com.wurmonline.mesh.Tiles;
+import com.wurmonline.server.skills.Skill;
 import com.wurmonline.server.bodys.BodyTemplate;
 import com.wurmonline.server.combat.ArmourTemplate;
 import com.wurmonline.server.combat.CombatMove;
@@ -8,56 +12,46 @@ import com.wurmonline.server.bodys.Wound;
 import com.wurmonline.server.creatures.CreatureTemplate;
 import com.wurmonline.server.creatures.CreatureTemplateFactory;
 import com.wurmonline.server.creatures.CreatureTemplateIds;
+
 import com.wurmonline.shared.constants.ItemMaterials;
-import com.wurmonline.server.creatures.NoSuchCreatureTemplateException;
-import com.wurmonline.server.skills.Skill;
 import com.wurmonline.shared.constants.CreatureTypes;
-import com.wurmonline.shared.constants.SoundNames;
+
 import org.feudalrealms.wurmunlimited.feudalcreatures.FeudalCreatures;
+
 import org.gotti.wurmunlimited.modsupport.CreatureTemplateBuilder;
 import org.gotti.wurmunlimited.modsupport.creatures.EncounterBuilder;
 import org.gotti.wurmunlimited.modsupport.creatures.ModCreature;
-
-import java.util.Map;
-import java.util.logging.Logger;
 
 import static com.wurmonline.server.skills.SkillList.*;
 import static com.wurmonline.server.items.ItemList.*;
 
 
-public class TrollWitchDoctor implements ModCreature, CreatureTypes, SoundNames {
+public class TrollWitchDoctor implements ModCreature, CreatureTypes, ItemMaterials {
     private int templateId;
-    private Map<Integer, Skill> baseSkillTree;
+//    private Map<Integer, Skill> baseSkillTree;
     private static final Logger LOGGER = Logger.getLogger(FeudalCreatures.class.getName());
 
     @Override
     public CreatureTemplateBuilder createCreateTemplateBuilder() {
-        this.getBaseTemplate();
 
-        int[] types = { C_TYPE_MOVE_GLOBAL,
-                C_TYPE_HUMAN,
-                C_TYPE_AGG_HUMAN,
-                C_TYPE_SWIMMING,
-                C_TYPE_HUNTING,
-                C_TYPE_CARNIVORE,
-                C_TYPE_NON_NEWBIE };
+
+        int[] types = { C_TYPE_MOVE_GLOBAL,C_TYPE_HUMAN,C_TYPE_AGG_HUMAN,C_TYPE_SWIMMING,C_TYPE_HUNTING,C_TYPE_CARNIVORE,C_TYPE_NON_NEWBIE };
 
         final int[] itemsButchered = {leather, cookedMeat, farmersSalve, potion};
 
-        float ogStrength = this.baseSkillTree.get(BODY_STRENGTH).getNumber();
-        float ogStamina = this.baseSkillTree.get(BODY_STRENGTH).getNumber();
-        float ogControl = this.baseSkillTree.get(BODY_CONTROL).getNumber();
+//        this.getBaseTemplate();
+//        float ogStrength = this.baseSkillTree.get(BODY_STRENGTH).getNumber();
+//        float ogStamina = this.baseSkillTree.get(BODY_STRENGTH).getNumber();
+//        float ogControl = this.baseSkillTree.get(BODY_CONTROL).getNumber();
 
         CreatureTemplateBuilder builder = new CreatureTemplateBuilder(
-                "feudalcreatures.trollwitchdoctor",
+                "mod.creature.trollwitchdoctor",
                 "Troll Witch Doctor",
-                "Dis wont hurt a bit...",
-                "model.creature.humanoid.troll.standard",
-                types,
-                BodyTemplate.TYPE_ETTIN, (short)5, (byte)0, (short)360, (short)20, (short)35,
-                DEATH_TROLL_SND, DEATH_TROLL_SND, HIT_TROLL_SND, HIT_TROLL_SND,
-                1.0F, 2.0F, 0.0F, 0.0F, 0.0F, 0.0F,
-                0.8F, 0, new int[0], 3, 0, (byte)80
+                "Dis wont hurt a bit...","model.creature.humanoid.troll.standard.witchdoctor",
+                types, BodyTemplate.TYPE_ETTIN, (short)5, (byte)0, (short)360, (short)20, (short)35,
+                "sound.death.troll", "sound.death.troll", "sound.combat.hit.troll", "sound.combat.hit.troll",
+                1.0F, 4.0F, 0.0F, 0.0F, 0.0F, 0.0F,
+                0.8F, 0, itemsButchered, 8, 40, MATERIAL_MEAT_HUMAN
         );
 
 //        CreatureTemplateBuilder builder = new CreatureTemplateBuilder("mod.creature.trollwitchdoctor") {
@@ -72,11 +66,10 @@ public class TrollWitchDoctor implements ModCreature, CreatureTypes, SoundNames 
 //        };
 
 //        builder.types(types);
-        builder.name("Troll Witch Doctor");
-        builder.description("Dis wont hurt a bit...");
+//        builder.name("Troll Witch Doctor");
+//        builder.description("Dis wont hurt a bit...");
 
         this.templateId = builder.getTemplateId();
-
         builder.skill(BODY_STRENGTH, 30.0F);
         builder.skill(BODY_CONTROL, 45.0F);
         builder.skill(BODY_STAMINA, 35.0F);
@@ -86,33 +79,15 @@ public class TrollWitchDoctor implements ModCreature, CreatureTypes, SoundNames 
         builder.skill(SOUL_DEPTH, 20.0F);
         builder.skill(SOUL_STRENGTH, 20.0F);
         builder.skill(WEAPONLESS_FIGHTING, 50.0f);
-
         builder.hasHands(true);
         builder.maxAge(100);
         builder.armourType(ArmourTemplate.ARMOUR_TYPE_STUDDED);
-        builder.baseCombatRating(25.0f);
-
+        builder.baseCombatRating(20.0f);
         builder.combatDamageType(Wound.TYPE_POISON);
-        builder.handDamString("whip");
         builder.handDamString("strike");
         builder.kickDamString("kick");
         builder.setCombatMoves(new int[]{CombatMove.STUN, CombatMove.DOUBLE_FIST});
-
-// SWEEP = 1 = " makes a circular powerful sweep!"
-// EARTHSHAKE = 2 = " shakes the earth!"
-// FIREBREATH = 3 =  " breathes fire!"
-// DOUBLE_FIST = 4 = " throws down @hisher powerful fists!"
-// STOMP = 5 = " stomps!",
-// THROW = 6 = " picks up and throws @defender!"
-// STUN = 7 = " stuns @defender!"
-// BASH = 8 = " bashes @defender!"
-// ACIDBREATH = 9 = " breathes acid!"
-// HELLHORSEFIRE = 10 = " breathes fire!"
-// PHASE = 11 =  " phases!"
-
         builder.maxPercentOfCreatures(0.04f);
-        builder.itemsButchered(itemsButchered);
-        builder.meatMaterial(ItemMaterials.MATERIAL_MEAT_HUMANOID);
 
         LOGGER.info("FeudalCreatures: Troll Witchdoctor");
         return builder;
@@ -120,26 +95,19 @@ public class TrollWitchDoctor implements ModCreature, CreatureTypes, SoundNames 
 
     @Override
     public void addEncounters() {
-        if (templateId == 0)
-            return;
+        if (templateId == 0) return;
 
-        new EncounterBuilder(Tiles.Tile.TILE_TREE.id)
-                .addCreatures(templateId, 1)
-                .build(1);
-        new EncounterBuilder(Tiles.Tile.TILE_GRASS.id)
-                .addCreatures(templateId, 1)
-                .build(1);
-        new EncounterBuilder(Tiles.Tile.TILE_CAVE.id, (byte) -1)
-                .addCreatures(templateId, 1)
-                .build(1);
+        new EncounterBuilder(Tiles.Tile.TILE_TREE.id).addCreatures(templateId, 1).build(1);
+        new EncounterBuilder(Tiles.Tile.TILE_GRASS.id).addCreatures(templateId, 1).build(1);
+        new EncounterBuilder(Tiles.Tile.TILE_CAVE.id, (byte) -1).addCreatures(templateId, 1).build(1);
     }
 
-    private void getBaseTemplate() {
-        try {
-            CreatureTemplate baseTemplate = CreatureTemplateFactory.getInstance().getTemplate(CreatureTemplateIds.TROLL_CID);
-            this.baseSkillTree = baseTemplate.getSkills().getSkillTree();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
+//    private void getBaseTemplate() {
+//        try {
+//            CreatureTemplate baseTemplate = CreatureTemplateFactory.getInstance().getTemplate(CreatureTemplateIds.TROLL_CID);
+//            this.baseSkillTree = baseTemplate.getSkills().getSkillTree();
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
 }
